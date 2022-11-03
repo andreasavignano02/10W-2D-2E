8db03c686444e5abdfddeff7817b62fd89a721e4
andreasavignano02 <112469730+andreasavignano02@users.noreply.github.com>
2022-11-03 08:42:44 +0100

    fine

--- esercizio2/src/app/page3/page3.component.spec.ts
+++ esercizio2/src/app/page3/page3.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MainComponent } from './main.component';
+import { Page3Component } from './page3.component';
 
-describe('MainComponent', () => {
-  let component: MainComponent;
-  let fixture: ComponentFixture<MainComponent>;
+describe('Page3Component', () => {
+  let component: Page3Component;
+  let fixture: ComponentFixture<Page3Component>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      declarations: [ Page3Component ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MainComponent);
+    fixture = TestBed.createComponent(Page3Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
